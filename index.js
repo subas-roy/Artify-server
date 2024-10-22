ec3d48a176797c958224a9cdf43a003a3e50c6be
subas-roy <subasroy46@gmail.com>
2024-10-22 22:40:08 +0600

    Get Single Craft Data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleware
@@ -31,6 +31,13 @@ async function run() {
     app.get('/crafts', async(req, res) => {
       const cursor = craftsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/crafts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await craftsCollection.findOne(query)
       res.send(result);
     })
 
