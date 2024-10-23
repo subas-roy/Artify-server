cf368ed0becea1a89f4567a65e9d228d7cc561ac
subas-roy <subasroy46@gmail.com>
2024-10-23 06:44:30 +0600

    Delete A Craft From The Database

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
       const result = await craftsCollection.insertOne(newCraft);
       res.send(result);
     })
+
+    app.delete('/crafts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await craftsCollection.deleteOne(query)
+      res.send(result);
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
